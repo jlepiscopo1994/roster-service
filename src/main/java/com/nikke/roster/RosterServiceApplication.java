f9dcc1a617292f60ea9b861182e4f3893bf817f9
jlepiscopo1994 <joeycentral@gmail.com>
2026-08-18 13:41:46 -0400

    feat: initial commit for roster-service scaffolding

---
+++ src/main/java/com/nikke/roster/RosterServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nikke.roster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RosterServiceApplication.class, args);
+    }
+}
